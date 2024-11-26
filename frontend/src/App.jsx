d28560c8a10f55538e24b28e18fdc0f148b9ab9f
Xen00n <nikunj11pokharel@gmail.com>
2024-11-26 19:00:34 +0545

    logo added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DbStatus from './components/DbStatus';
+import Logoo from './assets/logoo.png'
 
 function App() {
   return (
     <div className="container mx-auto mt-10">
-      <h1 className="text-2xl font-bold mb-4">damMilam</h1>
+      <h1 className="text-4xl font-bold mb-4">damMilam</h1>
+      <img src={Logoo} alt="logo" className='w-32 h-32'/>
       <DbStatus />
     </div>
   );
