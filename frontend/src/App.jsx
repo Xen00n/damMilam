f91f972db42370e96803bc5b1b584a1d9d37f5c9
Rambha123 <manandharprarambha00@gmail.com>
2024-11-28 18:18:40 +0545

    tailwaind changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container mx-auto mt-10">
       <h1 className="text-4xl font-bold mb-4">damMilam</h1>
-      <img src={Logoo} alt="logo" className='w-30 h-30'/>
+      <img src={Logoo} alt="logo" className='w-36 h-36'/>
       <DbStatus />
     </div>
   );
